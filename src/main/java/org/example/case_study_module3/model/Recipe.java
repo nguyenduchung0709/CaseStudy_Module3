94cfa700881a2b28855fe6719a03d33280d9fb54
ductr <probablydedrn@gmail.com>
2025-08-13 21:37:42 +0700

    java classes and recipe.jsp

--- src/main/java/org/example/case_study_module3/model/Recipe.java
+++ src/main/java/org/example/case_study_module3/model/Recipe.java
@@ -1,10 +1,27 @@
 package org.example.case_study_module3.model;
 
 public class Recipe {
+    private int id;
     private String name;
+    private String imageURL;
     private String ingredients; // nguyen lieu
     private String instructions; // huong dan
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 
     public String getName() {
         return name;
@@ -30,11 +47,13 @@ public class Recipe {
         this.instructions = instructions;
     }
 
-    public Recipe(String name, String ingredients, String instructions) {
+    public Recipe() {}
+
+    public Recipe(int id, String name, String imageURL, String ingredients, String instructions) {
+        this.id = id;
         this.name = name;
+        this.imageURL = imageURL;
         this.ingredients = ingredients;
         this.instructions = instructions;
     }
-
-    public Recipe() {}
 }
